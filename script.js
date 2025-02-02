4d32f52d0451f1944cd7dab7ca0bca69649e6767
Deepak Rizal <deepak2000rizal@gmail.com>
2025-02-02 14:18:57 +0530

    inplemented the functionalities like adding expense and listing expense

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const expenseForm = document.querySelector(".expense-form");
 const expenseDescription = document.getElementById("description");
 const expenseAmount = document.getElementById("amount");
 const expenseDate = document.getElementById("date");
+const submitButton = document.querySelector(".submit-expense");
+const expenseList = document.querySelector(".expense-list");
 
-const expense = {
+let expense = {
   description: "",
   amount: "",
   date: "",
@@ -11,8 +13,10 @@ const expense = {
 
 const expenses = [];
 
+functions;
+
 function inputValue(val, identifier) {
-  return { ...expense, [identifier]: val };
+  expense = { ...expense, [identifier]: val };
 }
 
 function valueHandler(e) {
@@ -20,6 +24,48 @@ function valueHandler(e) {
   inputValue(value, e.target.id);
 }
 
+function createElement(elementName, className, textContent) {
+  const element = document.createElement(elementName);
+  if (className) {
+    element.classList.add(className);
+  }
+  if (textContent) {
+    element.textContent = textContent;
+  }
+  return element;
+}
+
 expenseDescription.addEventListener("change", valueHandler);
 expenseAmount.addEventListener("change", valueHandler);
 expenseDate.addEventListener("change", valueHandler);
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (
+    expense.description.trim() === "" ||
+    expense.amount.trim() === "" ||
+    expense.date === ""
+  ) {
+    return alert("Please enter the expense: Description,Date and Amount");
+  }
+  expenses.push(expense);
+
+  let listItem;
+  expenses.forEach((expense) => {
+    listItem = createElement("li", "expense");
+    const expenseText = createElement("p", "expense-text");
+    expenseText.textContent = `Rs. ${expense.amount}`;
+    const buttons = createElement("div", "buttons");
+    const editButton = createElement("button", "edit", "Edit");
+    const deleteButton = createElement("button", "delete", "Delete");
+    buttons.append(editButton, deleteButton);
+    listItem.append(expenseText, buttons);
+  });
+
+  expenseList.appendChild(listItem);
+
+  expenseDescription.value = "";
+  expenseAmount.value = "";
+  expenseDate.value = "";
+});
